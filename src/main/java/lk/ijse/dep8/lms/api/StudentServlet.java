ed2bf089ea87f6ddcd12572ec9202684aa6cc582
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-04-21 16:06:39 +0530

    Add StudentServlet

---
+++ src/main/java/lk/ijse/dep8/lms/api/StudentServlet.java
@@ -0,0 +1,17 @@
+package lk.ijse.dep8.lms.api;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class StudentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
