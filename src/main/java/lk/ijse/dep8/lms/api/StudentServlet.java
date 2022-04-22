40b80840c01db9ce42bc449acae7a34e39663e14
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-04-22 18:25:10 +0530

    Add Cors Filter

--- src/main/java/lk/ijse/dep8/lms/api/StudentServlet.java
+++ src/main/java/lk/ijse/dep8/lms/api/StudentServlet.java
@@ -24,6 +24,20 @@ public class StudentServlet extends HttpServlet {
     private volatile DataSource pool;
 
     @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String origin = req.getHeader("Origin");
+//        if (origin.contains("localhost:1234")){
+//            resp.setHeader("Access-Control-Allow-Origin", origin);
+//            if (req.getMethod().equals("OPTIONS")) {
+//                resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD");
+//                resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//                resp.setHeader("Access-Control-Expose-Headers", "Content-Type");
+//            }
+//        }
+        super.service(req, resp);
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
 
